7d16c3a149ec9cf6bac90648ac5850b2046599cc
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-04-02 13:07:49 +0300

    add: default systems and adding tag components on init (ability tag and item tag)

--- ActorContainer/AbilityContainer.cs
+++ ActorContainer/AbilityContainer.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Components;
+using HECSFramework.Core;
+using UnityEngine;
 
 #if UNITY_EDITOR
 #endif
@@ -6,5 +8,12 @@
 namespace HECSFramework.Unity
 {
     [CreateAssetMenu(fileName = "AbilityContainer", menuName = "Ability Container")]
-    public class AbilityContainer : EntityContainer {}
+    public class AbilityContainer : EntityContainer 
+    {
+        public override void Init(IEntity entity)
+        {
+            base.Init(entity);
+            entity.AddHecsComponent(new AbilityTagComponent());
+        }
+    }
 }
--- ActorContainer/ItemContainer.cs
+++ ActorContainer/ItemContainer.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Components;
+using HECSFramework.Core;
+using UnityEngine;
 
 #if UNITY_EDITOR
 #endif
@@ -6,5 +8,12 @@
 namespace HECSFramework.Unity
 {
     [CreateAssetMenu(fileName = "ItemContainer", menuName = "Item Container")]
-    public class ItemContainer : EntityContainer {}
+    public class ItemContainer : EntityContainer 
+    {
+        public override void Init(IEntity entity)
+        {
+            base.Init(entity);
+            entity.AddHecsComponent(new ItemTagComponent());
+        }
+    }
 }
