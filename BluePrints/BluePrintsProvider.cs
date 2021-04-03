c94480209bc49934ac51376f6a11d5bd80037afc
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-04-03 03:48:47 +0300

    add: blue print provider partial part

---
+++ BluePrints/BluePrintsProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HECSFramework.Unity
+{
+    public partial class BluePrintsProvider
+	{
+		public Dictionary<Type, Type> Components;
+		public Dictionary<Type, Type> Systems;
+	}
+}
