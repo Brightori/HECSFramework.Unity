47b72c871fa5a9099b87df3515f46ee46f9bb82f
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-04-15 04:23:25 +0300

    add: data to entities helper - convert containers to entities

---
+++ Helpers/DataToEntitiesHelper.cs
@@ -0,0 +1,22 @@
+﻿using HECSFramework.Core;
+using System.Collections.Generic;
+
+namespace HECSFramework.Unity
+{
+    public static class DataToEntitiesHelper
+    {
+        public static void FillEntitiesList<T>(T[] containers, ref List<IEntity> entities, int worldIndex = 0) where T: EntityContainer 
+        {
+            if (entities.Count > 0)
+                return;
+
+            for (int i = 0; i < containers.Length; i++)
+            {
+                var container = containers[i];
+                var entity = new Entity(container.name, worldIndex);
+                container.Init(entity);
+                entities.Add(entity);
+            }
+        }
+    }
+}
