0e3921877236a39f7251261fe91ed642b49d0e37
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-04-16 03:07:58 +0300

    upd: now preset container can copy actual entity Container

--- ActorContainer/PresetContainer.cs
+++ ActorContainer/PresetContainer.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using HECSFramework.Unity.Editor;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 namespace HECSFramework.Unity
 {
@@ -8,5 +11,84 @@ namespace HECSFramework.Unity
     {
         public List<ComponentBluePrint> ComponentsBluePrints => holder.components;
         public List<SystemBaseBluePrint> SystemsBluePrints => holder.systems;
+
+        [Button(ButtonSizes.Large)]
+        public void MakePresetFromEntityContainer()
+        {
+            var window = UnityEditor.EditorWindow.GetWindow<MakePresetFromEntityContainer>();
+            window.Init(this);
+        }
     }
 }
+
+#if UNITY_EDITOR
+
+namespace HECSFramework.Unity.Editor
+{
+    using Sirenix.OdinInspector.Editor;
+    using UnityEditor;
+
+    public class MakePresetFromEntityContainer : OdinEditorWindow
+    {
+        [Searchable, AssetsOnly]
+        public EntityContainer presetContainer;
+        private EntityContainer currentContainer;
+
+        private bool inAction;
+
+        public void Init(EntityContainer actorContainer)
+        {
+            this.currentContainer = actorContainer;
+        }
+
+        [Button("Make clean copy", ButtonSizes.Medium), HideIf("@presetContainer == null")]
+        public void ReplaceContainerByPreset()
+        {
+            if (inAction)
+                return;
+
+            currentContainer.Clear();
+            AssetDatabase.SaveAssets();
+            Copy();
+        }
+
+        [Button("Copy Unique Data to Preset ", ButtonSizes.Medium), HideIf("@presetContainer == null")]
+        [PropertyTooltip("добавляем уникальные(актор будет иметь свобственный экземпляр блупринта) копии из пресета в актор")]
+        public void Copy()
+        {
+            if (inAction)
+                return;
+
+            inAction = true;
+
+            foreach (var componentBP in presetContainer.Components)
+            {
+                if (currentContainer.IsHaveComponentBlueprint(componentBP))
+                    continue;
+
+                var asset = Instantiate(componentBP);
+                AssetDatabase.AddObjectToAsset(asset, currentContainer);
+
+                asset.name = componentBP.GetHECSComponent.GetType().Name;
+                currentContainer.AddComponent(asset);
+            }
+
+            foreach (var sysBP in presetContainer.Systems)
+            {
+                if (currentContainer.IsHaveSystem(sysBP))
+                    continue;
+
+                var asset = Instantiate(sysBP);
+                AssetDatabase.AddObjectToAsset(asset, currentContainer);
+
+                asset.name = sysBP.GetSystem.GetType().Name;
+                currentContainer.AddSystem(asset);
+            }
+
+            AssetDatabase.SaveAssets();
+            Close();
+        }
+    }
+}
+
+#endif
