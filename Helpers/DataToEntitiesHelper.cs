639023d8a83c1779fa712e357e845363e2b9c3ec
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-04-23 08:58:54 +0300

    add: fill array of IEntity from, Array Entity containers

--- Helpers/DataToEntitiesHelper.cs
+++ Helpers/DataToEntitiesHelper.cs
@@ -1,4 +1,5 @@
 ﻿using HECSFramework.Core;
+using System;
 using System.Collections.Generic;
 
 namespace HECSFramework.Unity
@@ -16,6 +17,19 @@ namespace HECSFramework.Unity
                 var entity = new Entity(container.name, worldIndex);
                 container.Init(entity);
                 entities.Add(entity);
+            }
+        } 
+        
+        public static void FillEntitiesList<T>(T[] containers, ref IEntity[] entities, int worldIndex = 0) where T: EntityContainer 
+        {
+            entities = new IEntity[containers.Length];
+
+            for (int i = 0; i < containers.Length; i++)
+            {
+                var container = containers[i];
+                var entity = new Entity(container.name, worldIndex);
+                container.Init(entity);
+                entities[i] = entity;
             }
         }
     }
