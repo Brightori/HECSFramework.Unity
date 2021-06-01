ec2bf52067dffeb25d644ff00cadcd68a69d2f58
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-06-01 21:07:11 +0300

    add: getbasecomponent helper

---
+++ Helpers/GetComponentBase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+
+namespace Components
+{
+    public abstract class GetComponentBase<T> : MonoBehaviour where T : Component
+    {
+        private T component;
+
+        public T Get()
+        {
+            if (component != null)
+                return component;
+
+            component = GetComponent<T>();
+            Assert.IsNotNull(component, $"нет компонента Button у {gameObject.name}");
+
+            return component;
+        }
+    }
+}
