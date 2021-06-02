ed8c0f25cd479a26f33f5df3403b0cfb60db5bd6
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-06-03 01:19:45 +0300

    fix: resolvers path and directory creation

--- Editor/UnityProcessGeneration.cs
+++ Editor/UnityProcessGeneration.cs
@@ -36,7 +36,7 @@ namespace HECSFramework.Unity.Generator
             unityProcessGeneration.SaveToFile(ComponentContext, generator.GetComponentContext());
             unityProcessGeneration.SaveToFile(HecsMasks, generator.GenerateHecsMasks());
             unityProcessGeneration.SaveToFile(Documentation, generator.GetDocumentation());
-            
+
 
             //generate blue prints
             var componetsBPFiles = generator.GenerateComponentsBluePrints();
@@ -45,13 +45,13 @@ namespace HECSFramework.Unity.Generator
             unityProcessGeneration.SaveToFile(MapResolver, generator.GetResolverMap());
 
             foreach (var c in componetsBPFiles)
-                unityProcessGeneration.SaveToFile(c.name, c.classBody, ComponentsBluePrintsPath);  
-            
+                unityProcessGeneration.SaveToFile(c.name, c.classBody, ComponentsBluePrintsPath);
+
             foreach (var c in systemsBPFiles)
                 unityProcessGeneration.SaveToFile(c.name, c.classBody, SystemsBluePrintsPath);
-            
+
             foreach (var c in resolvers)
-                unityProcessGeneration.SaveToFile(c.name, c.content);
+                unityProcessGeneration.SaveToFile(c.name, c.content, DefaultPath + "Resolvers/");
 
             unityProcessGeneration.SaveToFile(BluePrintsProvider, generator.GetBluePrintsProvider(), needToImport: true);
         }
@@ -60,15 +60,22 @@ namespace HECSFramework.Unity.Generator
         {
             var path = dataPath + pathToDirectory + name;
 
-            if (!Directory.Exists(dataPath + DefaultPath))
-                Directory.CreateDirectory(dataPath + DefaultPath);
+            try
+            {
+                if (!Directory.Exists(Application.dataPath + pathToDirectory))
+                    Directory.CreateDirectory(Application.dataPath + pathToDirectory);
 
-            File.WriteAllText(path, data);
+                File.WriteAllText(path, data);
 
-            var sourceFile2 = path.Replace(Application.dataPath, "Assets");
-            
-            if (needToImport)
-                AssetDatabase.ImportAsset(sourceFile2);
+                var sourceFile2 = path.Replace(Application.dataPath, "Assets");
+
+                if (needToImport)
+                    AssetDatabase.ImportAsset(sourceFile2);
+            }
+            catch
+            {
+                Debug.LogError("не смогли ослить " + pathToDirectory);
+            }
         }
     }
 }
