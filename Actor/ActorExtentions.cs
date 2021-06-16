4f13cfcb7abe5206d8521f5f8794bfb175a2d7a3
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-06-17 02:18:09 +0300

    add: get actor functuionality

--- Actor/ActorExtentions.cs
+++ Actor/ActorExtentions.cs
@@ -1,6 +1,7 @@
 ﻿using Components;
 using HECSFramework.Core;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -29,6 +30,30 @@ namespace HECSFramework.Unity
             }
 
             throw new Exception("All going to second anus, because we dont have actor container id " + entity.ID);
+        }
+
+        public static async Task<IActor> GetActor(this ViewReferenceComponent viewReferenceComponent, Action<IActor> callBack = null)
+        {
+            var asynData = viewReferenceComponent.ViewReference.InstantiateAsync();
+            var actorPrfb = await asynData.Task;
+            Addressables.Release(asynData);
+            callBack?.Invoke(actorPrfb);
+            return actorPrfb;
+        }
+        
+        public static async Task<IActor> GetActor(this EntityContainer entityContainer, bool needLoadContainer = true,  Action<IActor> callBack = null)
+        {
+            var unpack = new UnpackContainer(entityContainer);
+            var viewReferenceComponent = (unpack.Components.FirstOrDefault(x => x is ViewReferenceComponent)) as ViewReferenceComponent;
+            var asynData = viewReferenceComponent.ViewReference.InstantiateAsync();
+            var actorPrfb = await asynData.Task;
+            Addressables.Release(asynData);
+
+            if (needLoadContainer)
+                unpack.InitEntity(actorPrfb);
+
+            callBack?.Invoke(actorPrfb);
+            return actorPrfb;
         }
     }
 }
