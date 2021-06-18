7989f046ba2a56d1cde0cbd9dce5f14255517eca
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-06-18 14:43:57 +0300

    add: resolver map section to unity part

---
+++ Helpers/ResolverMap.cs
@@ -0,0 +1,40 @@
+﻿using Components;
+using HECSFramework.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace HECSFramework.Core
+{
+    public partial class ResolversMap
+    {
+        public async Task<IEntity> GetEntityFromResolver(EntityResolver entityResolver, bool needInitFromContainer, bool needForceAdd = false, int worldIndex = 0)
+        {
+            var unpack = new UnPackEntityResolver(entityResolver);
+            var actorID = unpack.Components.FirstOrDefault(x => x is ActorContainerID containerID);
+
+            if (actorID == null)
+                return null;
+
+            var container = actorID as ActorContainerID;
+
+            IEntity entity;
+            entity = new Entity((actorID as ActorContainerID).ID, worldIndex);
+
+            if (needInitFromContainer)
+            {
+                var loaded = await Addressables.LoadAssetAsync<ScriptableObject>(container.ID).Task;
+                var loadedContainer = loaded as EntityContainer;
+                loadedContainer.Init(entity);
+                entity.LoadEntityFromResolver(entityResolver, needForceAdd);
+            }
+
+            entity.SetGuid(entityResolver.Guid);
+            return entity;
+        }
+    }
+}
