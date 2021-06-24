26a5bd7613b90b598d3399ccf03c90f954f9ce1e
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-06-24 04:13:20 +0300

    add: entity container asset reference

---
+++ AddressableCustomReference/EntityContainerReference.cs
@@ -0,0 +1,15 @@
+﻿using HECSFramework.Unity;
+using HECSFramework.Unity.Helpers;
+using System;
+using UnityEngine.AddressableAssets;
+
+namespace Helpers
+{
+    [Serializable]
+    public class EntityContainerReference : AssetReferenceT<EntityContainer>
+    {
+        public EntityContainerReference(string guid) : base(guid)
+        {
+        }
+    }
+}
