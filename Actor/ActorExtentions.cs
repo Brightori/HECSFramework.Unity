c2731c33f042794e0200a7f7618c066abc910c23
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-03 03:11:22 +0300

    udp: move serialization to diff submodule

--- Actor/ActorExtentions.cs
+++ Actor/ActorExtentions.cs
@@ -8,29 +8,9 @@ using UnityEngine.AddressableAssets;
 
 namespace HECSFramework.Unity
 {
-    public static class ActorExtentions
+    public static partial class ActorExtentions
     {
-        public static async Task<IActor> GetActor(this IEntity entity, Action<IActor> callBack =null)
-        {
-            if (entity == null)
-                throw new Exception("entity == null");
-
-            var save = new EntityResolver().GetEntityResolver(entity);
-
-            if (entity.TryGetHecsComponent(out ActorContainerID container))
-            {
-                var actorContainer = await Addressables.LoadAssetAsync<ScriptableObject>(container.ID).Task;
-                var data = entity.GetHECSComponent<ViewReferenceComponent>().ViewReference.InstantiateAsync();
-                var actorPrfb = await data.Task;
-                (actorContainer as EntityContainer).Init(actorPrfb);
-                await actorPrfb.LoadEntityFromResolver(save);
-                Addressables.Release(data);
-                callBack?.Invoke(actorPrfb);
-                return actorPrfb;
-            }
-
-            throw new Exception("All going to second anus, because we dont have actor container id " + entity.ID);
-        }
+        
 
         public static async Task<IActor> GetActor(this ViewReferenceComponent viewReferenceComponent, Action<IActor> callBack = null)
         {
