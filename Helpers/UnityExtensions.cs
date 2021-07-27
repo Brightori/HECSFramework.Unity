f376c9e58e8bff3fbc73cf2a55885d686096ce1d
Andrey Zuykov <a.zuykov@kefirgames.com>
2021-07-27 14:34:43 +0300

    upd: vector extensions

--- Helpers/UnityExtensions.cs
+++ Helpers/UnityExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using HECSFramework.Core;
 using UnityEngine;
 
 namespace HECSFramework.Unity.Helpers
@@ -10,6 +11,15 @@ namespace HECSFramework.Unity.Helpers
 
         public static Vector3 AsV3(this Vector2 source, float height = 0)
             => new Vector3(source.x, height, source.y);
+        
+        public static System.Numerics.Vector3 AsNumericsV3(this Vector3 source, float height = 0)
+            => new System.Numerics.Vector3(source.x, source.y, source.z);
+        
+        public static System.Numerics.Vector2 AsNumericsV2(this Vector2 source, float height = 0)
+            => new System.Numerics.Vector2(source.x, source.y);
+
+        public static Vector3Serialize AsSerializedV3(this Vector3 source, float height = 0)
+            => new Vector3Serialize(source.x, source.y, source.z);
         
         public static void RemoveDestroyedValues<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
             where TValue : Object
