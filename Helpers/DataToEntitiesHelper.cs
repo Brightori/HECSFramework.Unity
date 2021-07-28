a2e7e229509866d795b0859afe21cdcac0e37bb4
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-28 14:55:45 +0300

    upd: data to entities helper now supports list of containers

--- Helpers/DataToEntitiesHelper.cs
+++ Helpers/DataToEntitiesHelper.cs
@@ -25,6 +25,27 @@ namespace HECSFramework.Core
                 container.Init(entity);
                 entities.Add(entity);
             }
+        }  
+        
+        public static void FillEntitiesList<T>(List<T> containers, ref List<IEntity> entities, IEntity owner, int worldIndex = 0) where T: EntityContainer 
+        {
+            if (entities.Count > 0)
+                return;
+
+            for (int i = 0; i < containers.Count; i++)
+            {
+                if (containers[i] == null)
+                {
+                    Debug.LogAssertion("пустой контейнер у " + owner.ID);
+                    continue;
+                }
+
+                var container = containers[i];
+                var entity = new Entity(container.name, worldIndex);
+                entity.GenerateGuid();
+                container.Init(entity);
+                entities.Add(entity);
+            }
         } 
         
         public static void FillEntitiesList<T>(T[] containers, ref IEntity[] entities, IEntity owner, int worldIndex = 0) where T: EntityContainer 
