db8fb94053bd448ea48b00f979c3891ad11fa6b9
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-10-27 12:19:06 +0300

    rem: legacy staff

--- DefaultComponents/ViewDestructionDelayedComponent.cs
+++ DefaultComponents/ViewDestructionDelayedComponent.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using HECSFramework.Core;
-using Helpers;
+﻿using HECSFramework.Core;
+using HECSFramework.Documentation;
+using System;
 
 namespace Components
 {
--- Editor/UnityProcessGeneration.cs
+++ Editor/UnityProcessGeneration.cs
@@ -10,18 +10,8 @@ namespace HECSFramework.Unity.Generator
         public const string BluePrint = "BluePrint";
 
         private const string DefaultPath = "/Scripts/HECSGenerated/";
-        private const string ComponentsBluePrintsPath = "/Scripts/BluePrints/ComponentsBluePrints/";
-        private const string SystemsBluePrintsPath = "/Scripts/BluePrints/SystemsBluePrint/";
         private string dataPath = Application.dataPath;
-
-        private const string TypeProvider = "TypeProvider.cs";
-        private const string MaskProvider = "MaskProvider.cs";
-        private const string HecsMasks = "HECSMasks.cs";
-        private const string SystemBindings = "SystemBindings.cs";
-        private const string ComponentContext = "ComponentContext.cs";
-        private const string BluePrintsProvider = "BluePrintsProvider.cs";
         private const string Documentation = "Documentation.cs";
-        private const string MapResolver = "MapResolver.cs";
 
         [MenuItem("HECS Options/Generate Documentation")]
         public static void CodogenDocumentation()
@@ -31,41 +21,6 @@ namespace HECSFramework.Unity.Generator
             generator.GatherAssembly();
             unityProcessGeneration.SaveToFile(Documentation, generator.GetDocumentation(), needToImport: true);
         }
-
-        //[MenuItem("HECS Options/HECS Codogen")]
-        public static void UnityCodogen()
-        {
-            var generator = new CodeGenerator();
-            var unityProcessGeneration = new UnityProcessGeneration();
-            generator.GatherAssembly();
-            unityProcessGeneration.SaveToFile(TypeProvider, generator.GenerateTypesMap());
-            unityProcessGeneration.SaveToFile(MaskProvider, generator.GenerateMaskProvider());
-            unityProcessGeneration.SaveToFile(SystemBindings, generator.GetSystemBinds());
-            unityProcessGeneration.SaveToFile(ComponentContext, generator.GetComponentContext());
-            unityProcessGeneration.SaveToFile(HecsMasks, generator.GenerateHecsMasks());
-          
-
-
-            //generate blue prints
-            var componetsBPFiles = generator.GenerateComponentsBluePrints();
-            var systemsBPFiles = generator.GenerateSystemsBluePrints();
-            var resolvers = generator.GetSerializationResolvers();
-            unityProcessGeneration.SaveToFile(MapResolver, generator.GetResolverMap());
-
-            foreach (var c in componetsBPFiles)
-                unityProcessGeneration.SaveToFile(c.name, c.classBody, ComponentsBluePrintsPath);
-
-            foreach (var c in systemsBPFiles)
-                unityProcessGeneration.SaveToFile(c.name, c.classBody, SystemsBluePrintsPath);
-
-            foreach (var c in resolvers)
-                unityProcessGeneration.SaveToFile(c.name, c.content, DefaultPath + "Resolvers/");
-
-            unityProcessGeneration.SaveToFile(BluePrintsProvider, generator.GetBluePrintsProvider(), needToImport: true);
-            unityProcessGeneration.GenerateNetworkStaff();
-        }
-
-        partial void GenerateNetworkStaff();
 
         private void SaveToFile(string name, string data, string pathToDirectory = DefaultPath, bool needToImport = false)
         {
