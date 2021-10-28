ecde2e217909ecd4215d69eff73ce2528fc994ac
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-10-28 14:19:29 +0300

    add: rewards blueprints

---
+++ Rewards/RewardsBluePrint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace HECSFramework.Rewards
+{
+    public abstract class RewardsBluePrintBase : ScriptableObject
+    {
+        public abstract IReward GetReward();
+    }
+
+    public abstract class RewardsBluePrint<RewardType> : RewardsBluePrintBase where RewardType : IReward
+    {
+        [SerializeField] RewardType Reward;
+
+        public override IReward GetReward() => Reward;
+    }
+}
