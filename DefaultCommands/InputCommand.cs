b90cdcad5e0994e8503ce939b0fa594cf0d65615
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-11-24 03:44:21 +0300

    fix: input system and binding input component add: input identifier

--- DefaultCommands/InputCommand.cs
+++ DefaultCommands/InputCommand.cs
@@ -8,18 +8,21 @@ namespace Commands
     [Documentation(Doc.Input, "Обозначает нажатие на кнопку (или что-то другое в зависимости от конфигурации InputAction)")]
     public struct InputStartedCommand : IGlobalCommand
     {
+        public int Index;
         public InputAction.CallbackContext Context { get; set; }
     }
 
     [Documentation(Doc.Input, "Обозначает отпускание кнопки (или что-то другое в зависимости от конфигурации InputAction)")]
     public struct InputEndedCommand : IGlobalCommand
     {
+        public int Index;
         public InputAction.CallbackContext Context { get; set; }
     }
 
     [Documentation(Doc.Input, "Обозначает удерживание кнопки (или что-то другое в зависимости от конфигурации InputAction)")]
     public struct InputCommand : IGlobalCommand
     {
+        public int Index;
         public InputAction.CallbackContext Context { get; set; }
     }
 }
--- DefaultComponents/InputActionsComponent.cs
+++ DefaultComponents/InputActionsComponent.cs
@@ -1,19 +1,128 @@
-﻿using System;
-using HECSFramework.Core;
+﻿using HECSFramework.Core;
 using HECSFramework.Documentation;
 using HECSFramework.Unity;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Utilities;
 
 namespace Components
 {
-    [Documentation(Doc.Input, "Содержит информацию о настройках ввода Unity.")]
+    [Documentation(Doc.Input, "Содержит информацию о настройках ввода Unity")]
     [Serializable, BluePrint]
     public class InputActionsComponent : BaseComponent
     {
-        [SerializeField] private InputActionMap actions;
+        [SerializeField] private InputActionAsset actions;
+        [SerializeField, ListDrawerSettings(ShowPaging = false)]
+        private List<InputActionSettings> inputActionSettings = new List<InputActionSettings>();
 
-        public ReadOnlyArray<InputAction> Actions => actions.actions;
+        public InputActionAsset Actions=> actions;
+        public ReadonlyList<InputActionSettings> InputActionSettings;
+
+        protected override void ConstructorCall()
+        {
+            InputActionSettings = new ReadonlyList<InputActionSettings>(inputActionSettings);
+        }
+
+        #region UnityEditor
+#if UNITY_EDITOR
+        private string savePath => "Assets/" + "/Blueprints/" + "Identifiers/" + "InputIdentifiers/";
+
+        [Button]
+        private void FillInputActions()
+        {
+            var hashTest = new HashSet<string>();
+
+            foreach (var map in actions.actionMaps)
+            {
+                foreach (var action in map.actions)
+                {
+                    var check = inputActionSettings.FirstOrDefault(x => x.ActionName == action.name);
+
+                    if (check == null)
+                        inputActionSettings.Add(new InputActionSettings { ActionName = action.name });
+
+                    hashTest.Add(action.name);
+                }
+            }
+
+            foreach (var inputAction in inputActionSettings.ToArray())
+            {
+                if (hashTest.Contains(inputAction.ActionName)) continue;
+                else inputActionSettings.Remove(inputAction);
+            }
+
+            CreateAndFillIdentifiers();
+        }
+
+        private void CreateAndFillIdentifiers()
+        {
+            var inputIdentifiers = AssetDatabase.FindAssets("t:InputIdentifier")
+               .Select(x => UnityEditor.AssetDatabase.GUIDToAssetPath(x))
+               .Select(x => UnityEditor.AssetDatabase.LoadAssetAtPath<InputIdentifier>(x)).ToList();
+
+            CheckFolder(savePath);
+
+            foreach (var inputAction in inputActionSettings)
+            {
+                if (inputAction.Identifier == null || inputAction.Identifier.name != inputAction.ActionName)
+                {
+                    var neededIdentifier = inputIdentifiers.FirstOrDefault(x => x.name == inputAction.ActionName);
+
+                    if (neededIdentifier != null)
+                    {
+                        inputAction.Identifier = neededIdentifier;
+                        continue;
+                    }
+
+                    inputAction.Identifier = CreateIdentifier(inputAction.ActionName);
+                }
+            }
+
+            AssetDatabase.SaveAssets();
+        }
+
+        private InputIdentifier CreateIdentifier(string name)
+        {
+            var identifier = ScriptableObject.CreateInstance<InputIdentifier>();
+            identifier.name = name;
+
+            SaveIdentifier(identifier);
+            return identifier;
+        }
+
+        private void SaveIdentifier(InputIdentifier inputIdentifier)
+        {
+            AssetDatabase.CreateAsset(inputIdentifier, savePath + $"{inputIdentifier.name}.asset");
+        }
+
+        private static void CheckFolder(string path)
+        {
+            var folder = new DirectoryInfo(path);
+
+            if (folder == null || !folder.Exists)
+                Directory.CreateDirectory(path);
+        }
+
+#endif
+        #endregion
+    }
+
+    [Serializable]
+    public class InputActionSettings
+    {
+        [ReadOnly]
+        public string ActionName;
+
+        [ReadOnly]
+        public InputIdentifier Identifier;
+
+        //public InputAction InputAction;
     }
 }
