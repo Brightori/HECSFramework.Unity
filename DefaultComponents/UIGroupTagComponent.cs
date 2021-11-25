9ef5cf5aea4ea064aa39fd45f0766d2d3a3f4b67
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-11-25 04:13:51 +0300

    add: ui funcs

---
+++ DefaultComponents/UIGroupTagComponent.cs
@@ -0,0 +1,16 @@
+﻿using HECSFramework.Core;
+using HECSFramework.Documentation;
+using HECSFramework.Unity;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Components
+{
+    [Serializable, Documentation(Doc.UI, "Компонент тег, который мы вешаем на UI и таким образом обозначаем принадлежность как какой группе, может содержать несколько тегов")]
+    public class UIGroupTagComponent : BaseComponent
+    {
+        [SerializeField] private UIGroupIdentifier[] Groups = new UIGroupIdentifier[0];
+        public bool IsHaveGroupIndex (int index) => Groups.Any(x => x.Id == index);
+    }
+}
---
+++ IdentifierContainer/UIGroupIdentifier.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace HECSFramework.Unity
+{
+    [CreateAssetMenu(fileName = "identifier", menuName = "Identifiers/UI Group Identifier")]
+    public class UIGroupIdentifier : IdentifierContainer { }
+}
