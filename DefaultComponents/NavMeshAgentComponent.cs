93efcc96cf2a0f6e3b0b02d73d40752c881804ab
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-12-17 14:06:52 +0300

    add: default navmesh agent component

---
+++ DefaultComponents/NavMeshAgentComponent.cs
@@ -0,0 +1,29 @@
+﻿using HECSFramework.Core;
+using HECSFramework.Unity;
+using HECSFramework.Unity.Helpers;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Components
+{
+    [Serializable, BluePrint]
+    public class NavMeshAgentComponent : BaseComponent, IHaveActor, IInitable
+    {
+        private LazyMonoBehComponent<NavMeshAgent> navmeshAgent;
+        public NavMeshAgent NavMeshAgent => navmeshAgent.GetComponent();
+
+        public IActor Actor { get; set; }
+
+        public void Init()
+        {
+            navmeshAgent = new LazyMonoBehComponent<NavMeshAgent>(Owner);
+        }
+
+        public void SetDestination(Vector3 destination)
+        {
+            NavMeshAgent.SetDestination(destination);
+            NavMeshAgent.isStopped = false;
+        }
+    }
+}
