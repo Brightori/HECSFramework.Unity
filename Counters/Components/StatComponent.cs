fdfdcec3c4c9087be4716659d222d21c6109f89d
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-04-19 02:38:24 +0300

    upd: roslyn codogen add stat component

---
+++ Counters/Components/StatComponent.cs
@@ -0,0 +1,20 @@
+﻿using HECSFramework.Unity;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Components
+{
+    public abstract class StatComponent<T> : ModifiableFloatCounterComponent where T : IdentifierContainer
+    {
+        [SerializeField] protected float startValue = 10;
+        [SerializeField] private T counterIdentifier;
+
+        public override int Id => counterIdentifier.Id;
+        public override float SetupValue => startValue;
+
+        [ShowInInspector] //this property for debing|show at inspector
+        public float ShowCurrentValue => modifiersContainer != null ? modifiersContainer.CurrentValue : startValue;
+
+        
+    }
+}
