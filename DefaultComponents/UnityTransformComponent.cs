95471c18ff7ac564cb1b9a81da7d884cec4e6d26
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-04-27 16:34:11 +0300

    add: unity transform provider

---
+++ DefaultComponents/UnityTransformComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HECSFramework.Core;
+using HECSFramework.Unity;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Components
+{
+    [Serializable]
+    [Documentation(Doc.HECS, Doc.Actor, "This component provide access to unity transform, if this entity not actor, this component remove self")]
+    public sealed class UnityTransformComponent : BaseComponent, IHaveActor, IInitable
+    {
+        public IActor Actor { get; set; }
+        
+        [ReadOnly]
+        public Transform Transform;
+
+        public void Init()
+        {
+            if (Actor != null)
+                Transform = Actor.GameObject.GetComponent<Transform>();
+            else
+                Actor.RemoveHecsComponent(this);
+        }
+    }
+}
