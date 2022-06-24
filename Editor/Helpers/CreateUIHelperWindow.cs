96c35a9e1a132f0c757c7495e6c8462fc7a36b74
DESKTOP-11GN4NS\User <evgenii.dubovik@playstrom.io>
2022-06-24 12:23:38 +0300

    upd: create ui window in progress

--- Editor/Helpers/CreateUIHelperWindow.cs
+++ Editor/Helpers/CreateUIHelperWindow.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using HECSFramework.Core;
 using HECSFramework.Unity;
 using HECSFramework.Unity.Editor;
@@ -6,11 +8,15 @@ using Helpers;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
 using UnityEditor;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
 using UnityEngine;
 
-[Documentation(Doc.Editor, Doc.HECS, "Its helper for create ui, this window create ui identifier, ui blueprint, and set ui actor to prfb if needed, after this ui blueprint and uiactor will be added to addressables")]
+[Documentation(Doc.Editor, Doc.HECS, Doc.UI, "Its helper for create ui, this window create ui identifier, ui blueprint, and set ui actor to prfb if needed, after this ui blueprint and uiactor will be added to addressables")]
 public class CreateUIHelperWindow : OdinEditorWindow
 {
+    private const string UIBluePrintsGroup = "UIBluePrints";
+    private const string UIActors = "UIBluePrints";
+
     public string IdentifierName;
     public GameObject UIprfb;
     public UIGroupIdentifier[] Groups;
@@ -30,6 +36,12 @@ public class CreateUIHelperWindow : OdinEditorWindow
         InstallHECS.CheckFolder(pathBluePrints);
         InstallHECS.CheckFolder(pathUIIdentifiers);
 
+        if (string.IsNullOrEmpty(IdentifierName))
+        {
+            this.ShowNotification(new GUIContent("Identifier name not setted properly"));
+            return;
+        }
+
         if (File.Exists(pathUIIdentifiers + $"{IdentifierName}.asset"))
         {
             this.ShowNotification(new GUIContent("We alrdy have identifier like this"));
@@ -42,8 +54,8 @@ public class CreateUIHelperWindow : OdinEditorWindow
             return;
         }
 
-        var uiidentifier = ScriptableObject.CreateInstance<UIIdentifier>();
-        var uibluePrint = ScriptableObject.CreateInstance<UIBluePrint>();
+        var uiidentifier = CreateInstance<UIIdentifier>();
+        var uibluePrint = CreateInstance<UIBluePrint>();
 
         uiidentifier.name = IdentifierName;
         uibluePrint.name = $"{UIprfb.name}_UIBluePrint";
@@ -51,21 +63,33 @@ public class CreateUIHelperWindow : OdinEditorWindow
         AssetDatabase.CreateAsset(uibluePrint, InstallHECS.Assets + InstallHECS.BluePrints + InstallHECS.UIBluePrints + $"{uibluePrint.name}.asset");
         AssetDatabase.CreateAsset(uiidentifier, InstallHECS.Assets + InstallHECS.BluePrints + InstallHECS.Identifiers+InstallHECS.UIIdentifiers + $"{uiidentifier.name}.asset");
 
-        
-
         var actor = UIprfb.GetOrAddMonoComponent<UIActor>();
         var path = AssetDatabase.GetAssetPath(UIprfb);
-
-        //PrefabUtility.ApplyAddedComponent(actor, path, InteractionMode.AutomatedAction);
-        //var components = PrefabUtility.GetAddedComponents(t);
-
-        //foreach (var c in components)
-        //{
-
-        //    var ct = c.GetAssetObject();
-        //    //if (c.GetAssetObject() != null)
-        //}
-
         AssetDatabase.SaveAssets();
+
+        var addressablesSettings = UnityEditor.AddressableAssets.AddressableAssetSettingsDefaultObject.Settings;
+        var addressablesSchemas = CreateInstance<PlayerDataGroupSchema>();
+
+        var uiactorsGroup = addressablesSettings.groups.FirstOrDefault(x => x.name == UIActors);
+        var uiBluePrintsGroup = addressablesSettings.groups.FirstOrDefault(x => x.name == UIBluePrintsGroup);
+
+        if (uiactorsGroup == null)
+        {
+            uiactorsGroup = addressablesSettings.CreateGroup(UIActors, false, false, false, 
+                new List<UnityEditor.AddressableAssets.Settings.AddressableAssetGroupSchema>() 
+                    { addressablesSchemas } , null);
+        }
+
+        if (uiBluePrintsGroup == null)
+        {
+            uiBluePrintsGroup = addressablesSettings.CreateGroup(UIActors, false, false, false, 
+                new List<UnityEditor.AddressableAssets.Settings.AddressableAssetGroupSchema>() 
+                    { addressablesSchemas }, null);
+        }
+
+
+        var prfbGuid = AssetDatabase.GUIDFromAssetPath(path);
+        var entry = addressablesSettings.CreateOrMoveEntry(prfbGuid.ToString(), uiactorsGroup);
+
     }
 }
