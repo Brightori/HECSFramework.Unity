9c405624e7a71b5410654e474a0d8068f61ddb8c
Brightori <el.brightori@gmail.com>
2022-06-30 22:22:45 +0300

    upd: ui system

---
+++ UI/Components/AdditionalCanvasTagComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using HECSFramework.Core;
+using HECSFramework.Unity;
+
+namespace Components
+{
+    [Serializable, Documentation(Doc.UI, Doc.Tag, "This component holds identifier for additional canvases, we use this canvases for optimisation or drawing purpose")]
+    public sealed class AdditionalCanvasTagComponent : BaseComponent
+    {
+        public AdditionalCanvasIdentifier AdditionalCanvasIdentifier;
+    }
+}
