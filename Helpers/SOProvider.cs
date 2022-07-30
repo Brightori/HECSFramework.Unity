671b6557083e2de0e37a4a54e54095cada4a1034
Brightori <el.brightori@gmail.com>
2022-07-31 02:41:12 +0300

    add: SO provider, this helper gather all SO of needed type and return IEnumerable

---
+++ Helpers/SOProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HECSFramework.Core;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace HECSFramework.Unity.Helpers
+{
+    [Documentation(Doc.HECS, Doc.Helpers, "this helper gather all SO of needed type and return IEnumerable, its useful for drop down menus")]
+    public class SOProvider<T> where T: ScriptableObject
+    {
+        public IEnumerable<T> GetNeeded()
+        {
+#if UNITY_EDITOR
+            var containers = AssetDatabase.FindAssets($"t: {typeof(T).Name}")
+            .Select(x => UnityEditor.AssetDatabase.GUIDToAssetPath(x))
+            .Select(x => UnityEditor.AssetDatabase.LoadAssetAtPath<T>(x)).ToList();
+
+            return containers;
+#endif
+#if UNITY_2017_1_OR_NEWER
+            return Enumerable.Empty<T>();
+#endif
+        }
+    }
+}
