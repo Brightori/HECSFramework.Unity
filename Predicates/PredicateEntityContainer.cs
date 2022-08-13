976aafa804f98cc6e7112935e12a011da61f1fee
Brightori <el.brightori@gmail.com>
2022-08-14 02:29:16 +0300

    add: predicates for entity containers

---
+++ Predicates/PredicateEntityContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HECSFramework.Core;
+
+namespace HECSFramework.Unity
+{
+    public interface IPredicateEntityContainer
+    {
+        bool IsReady(EntityContainer target, IEntity owner = null);
+    }
+}
