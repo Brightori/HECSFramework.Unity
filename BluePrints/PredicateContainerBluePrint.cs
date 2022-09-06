a08a552a27832cd277488dda400862f244a0902d
Brightori <el.brightori@gmail.com>
2022-09-07 01:30:07 +0300

    add: predicate container blueprint

---
+++ BluePrints/PredicateContainerBluePrint.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace HECSFramework.Unity
+{
+    public abstract class PredicateContainerBluePrint : ScriptableObject
+    {
+        public abstract IPredicateEntityContainer GetPredicate { get; }
+    }
+}
