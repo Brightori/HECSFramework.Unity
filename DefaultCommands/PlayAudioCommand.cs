20c33a53f151e01eb393056fd2b50891b0374b85
Brightori <el.brightori@gmail.com>
2022-09-12 01:33:31 +0300

    add: default sound solution

---
+++ DefaultCommands/PlayAudioCommand.cs
@@ -0,0 +1,23 @@
+﻿using HECSFramework.Core;
+using System;
+using Systems;
+using UnityEngine;
+
+namespace Commands
+{
+    [Documentation(Doc.Audio, "command to play sound")]
+    public struct PlayAudioCommand : IGlobalCommand
+    {
+        public Guid Owner;
+        public AudioClip Clip;
+        public SoundType AudioType;
+        public bool IsRepeatable;
+    }
+
+    [Documentation(Doc.Audio, "command to stop sound")]
+    public struct StopAudioCommand : IGlobalCommand
+    {
+        public Guid Owner;
+        public AudioClip Clip;
+    }
+}
---
+++ DefaultSystems/SoundGlobalSystem.cs
@@ -0,0 +1,228 @@
+﻿using Commands;
+using DG.Tweening;
+using HECSFramework.Core;
+using HECSFramework.Unity;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Systems
+{
+    [Serializable, BluePrint]
+    [Documentation(Doc.Audio, Doc.Global, Doc.HECS, "Default sound solution for HECS, its all about 2d sound on this moment")]
+    public class SoundGlobalSystem : BaseSystem, ISoundGlobalSystem, IHaveActor, IReactEntity,
+        IReactGlobalCommand<PlayAudioCommand>,
+        IReactGlobalCommand<StopAudioCommand>
+    {
+        [SerializeField] private AudioMixerGroup audioMixerGroup;
+
+        private List<SoundSourceContainer> SoundSources = new List<SoundSourceContainer>(32);
+
+        public IActor Actor { get; set; }
+
+        public override void InitSystem()
+        {
+            Actor.TryGetComponents<AudioSource>(out var soundSources);
+
+            if (soundSources != null)
+            {
+                foreach (var s in soundSources)
+                    SoundSources.Add(new SoundSourceContainer(s));
+            }
+            else
+                Debug.LogAssertion("нет аудио сорсов у актора глобальной аудио системы");
+
+            SetupSoundGroup();
+
+            if (Actor.TryGetComponent(out DefaultMusicMonoComponent defaultMusicMonoComponent))
+            {
+                CommandGlobalReact(new PlayAudioCommand { AudioType = SoundType.Music, IsRepeatable = true, Clip = defaultMusicMonoComponent.AudioClip, Owner = this.Owner.GUID });
+            }
+        }
+
+        void SetupSoundGroup()
+        {
+            foreach (var go in SoundSources)
+                go.AudioSource.loop = false;
+        }
+
+        private void PlaySound(PlayAudioCommand playAudioCommand)
+        {
+            if (playAudioCommand.Clip == null)
+                return;
+
+            if (playAudioCommand.AudioType == SoundType.Music)
+            {
+                PlayMusic(playAudioCommand);
+                return;
+            }
+
+            for (int i = 0; i < SoundSources.Count; i++)
+            {
+                SoundSourceContainer soundSource = SoundSources[i];
+
+                if (!soundSource.IsBusy)
+                {
+                    var source = soundSource.AudioSource;
+                    soundSource.IsRepeatable = playAudioCommand.IsRepeatable;
+                    soundSource.IsBusy = true;
+                    soundSource.Owner = playAudioCommand.Owner;
+                    soundSource.AudioType = playAudioCommand.AudioType;
+
+                    source.volume = 1;
+                    source.clip = playAudioCommand.Clip;
+                    source.loop = playAudioCommand.IsRepeatable;
+                    source.Play();
+
+                    SoundSources[i] = soundSource;
+                    return;
+                }
+            }
+        }
+
+        private void PlayMusic(PlayAudioCommand playAudioCommand)
+        {
+            for (int i = 0; i < SoundSources.Count; i++)
+            {
+                SoundSourceContainer soundSource = SoundSources[i];
+
+                if (soundSource.AudioType == SoundType.Music)
+                {
+                    Stop(i);
+                    break;
+                }
+            }
+
+            if (playAudioCommand.IsRepeatable)
+            {
+                StopFromSource(playAudioCommand.Owner, playAudioCommand.Clip);
+            }
+
+            for (int i = 0; i < SoundSources.Count; i++)
+            {
+                SoundSourceContainer soundSource = SoundSources[i];
+
+                if (!soundSource.IsBusy)
+                {
+                    var source = soundSource.AudioSource;
+                    soundSource.IsRepeatable = playAudioCommand.IsRepeatable;
+                    soundSource.IsBusy = true;
+                    soundSource.Owner = playAudioCommand.Owner;
+                    soundSource.AudioType = playAudioCommand.AudioType;
+                    source.clip = playAudioCommand.Clip;
+                    source.loop = playAudioCommand.IsRepeatable;
+                    source.Play();
+                    source.DOFade(1, 1);
+
+                    SoundSources[i] = soundSource;
+                    return;
+                }
+            }
+        }
+
+        public void StopAllFromSource(Guid owner)
+        {
+            for (int i = 0; i < SoundSources.Count; i++)
+            {
+                var soundSource = SoundSources[i];
+                var index = i;
+
+                if (soundSource.Owner == owner)
+                {
+                    Stop(index);
+                }
+            }
+        }
+
+        public void StopFromSource(Guid owner, AudioClip sound)
+        {
+            for (int i = 0; i < SoundSources.Count; i++)
+            {
+                var soundSource = SoundSources[i];
+                var index = i;
+
+                if (soundSource.Owner == owner && soundSource.AudioSource.clip == sound)
+                {
+                    Stop(index);
+                    return;
+                }
+            }
+        }
+
+        public void EntityReact(IEntity entity, bool isAdded)
+        {
+            if (!isAdded)
+                StopAllFromSource(entity.GUID);
+        }
+
+        private void Stop(int indexOfSource)
+        {
+            SoundSources[indexOfSource].AudioSource.DOFade(0, 1).OnComplete(() => { SoundSources[indexOfSource].Stop(); });
+        }
+
+        public void CommandGlobalReact(PlayAudioCommand command)
+        {
+            PlaySound(command);
+        }
+
+        public void CommandGlobalReact(StopAudioCommand command)
+        {
+            StopFromSource(command.Owner, command.Clip);
+        }
+    }
+
+    public struct SoundSourceContainer
+    {
+        public AudioSource AudioSource;
+        public Guid Owner;
+        public SoundType AudioType;
+
+        public bool IsRepeatable;
+        private bool isBusy;
+
+        public bool IsBusy { get => isBusy && AudioSource.isPlaying; set => isBusy = value; }
+
+        public SoundSourceContainer(AudioSource audioSource) : this()
+        {
+            AudioSource = audioSource;
+        }
+
+        public void Stop()
+        {
+            AudioSource.Stop();
+            AudioSource.clip = null;
+            Owner = Guid.Empty;
+            isBusy = false;
+            IsRepeatable = false;
+            AudioType = SoundType.Default;
+            AudioSource.loop = false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SoundSourceContainer container &&
+                   EqualityComparer<AudioSource>.Default.Equals(AudioSource, container.AudioSource) &&
+                   Owner.Equals(container.Owner) &&
+                   AudioType == container.AudioType;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1844660361;
+            hashCode = hashCode * -1521134295 + EqualityComparer<AudioSource>.Default.GetHashCode(AudioSource);
+            hashCode = hashCode * -1521134295 + Owner.GetHashCode();
+            hashCode = hashCode * -1521134295 + AudioType.GetHashCode();
+            return hashCode;
+        }
+    }
+
+    public enum SoundType
+    {
+        Default = 0,
+        FX = 1,
+        Music = 2,
+    }
+
+    public interface ISoundGlobalSystem : ISystem { }
+}
