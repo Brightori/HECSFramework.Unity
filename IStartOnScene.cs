16d79280d09e405b487b36d96705ef000980f745
Brightori <el.brightori@gmail.com>
2022-09-25 02:44:22 +0300

    add: public interface IStartOnScene

--- IStartOnScene.cs
+++ IStartOnScene.cs
@@ -1,5 +1,9 @@
 ﻿namespace HECSFramework.Unity
 {
+    /// <summary>
+    /// this is special interface for cases when we load location to new world and need start some things 
+    /// after init root actors, bcz start event was alrdy passed and we need new one
+    /// </summary>
     public interface IStartOnScene
     {
         void StartOnScene();
