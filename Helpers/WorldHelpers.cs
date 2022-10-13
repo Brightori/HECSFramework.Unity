4e3de8a64661de54625636f582580474f85632cc
Brightori <el.brightori@gmail.com>
2022-10-14 01:28:39 +0300

    upd: revert world helper

--- Helpers/WorldHelpers.cs
+++ Helpers/WorldHelpers.cs
@@ -1,47 +1,29 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Cysharp.Threading.Tasks;
+using System.Threading.Tasks;
 using HECSFramework.Core;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.ResourceProviders;
-using UnityEngine.SceneManagement;
 
 namespace HECSFramework.Unity
 {
     [Documentation(Doc.HECS, "This helper load location to world")]
     public static partial class WorldHelpers
     {
-        // private static bool sceneIsLoaded;
-        public static async UniTask<SceneInstance> LoadLocationToWorld(this World world, AssetReference location)
+        public static async Task<SceneInstance> LoadLocationToWorld(this World world, AssetReference location)
         {
-            var scene = await Addressables.LoadSceneAsync(location, LoadSceneMode.Additive).Task;
+            var scene = await Addressables.LoadSceneAsync(location, UnityEngine.SceneManagement.LoadSceneMode.Additive).Task;
             InitGameObjects(world, scene.Scene.GetRootGameObjects());
             return scene;
         }
 
-        public static async UniTask LoadLocationToWorld(this World world, string sceneName)
+        public static async Task<SceneInstance> LoadLocationToWorld(this World world, string location)
         {
-            await SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-            var scene = SceneManager.GetSceneByName(sceneName);
-            InitGameObjects(world, scene.GetRootGameObjects());
+            var scene = await Addressables.LoadSceneAsync(location, UnityEngine.SceneManagement.LoadSceneMode.Additive).Task;
+            InitGameObjects(world, scene.Scene.GetRootGameObjects());
+            return scene;
         }
-        // public static async UniTask LoadLocationToWorld(this World world, string sceneName)
-        // {
-        //     sceneIsLoaded = false;
-        //     SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
-        //     SceneManager.sceneLoaded += OnSceneLoaded;
-        //
-        //     await UniTask.WaitUntil(() => sceneIsLoaded);
-        //     var scene = SceneManager.GetSceneByName(sceneName);
-        //     InitGameObjects(world, scene.GetRootGameObjects());
-        // }
-
-        // private static void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
-        // {
-            // SceneManager.sceneLoaded -= OnSceneLoaded;
-            // sceneIsLoaded = true;
-        // }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void InitGameObjects(World world, GameObject[] gameObjects)
@@ -63,15 +45,15 @@ namespace HECSFramework.Unity
                     a.Init(world.Index, true);
                     actorsList.Add(a);
                 }
-            }
 
-            foreach (var a in actorsList)
-            {
-                foreach (var s in a.GetAllSystems)
+                foreach (var a in actors)
                 {
-                    if (s is IStartOnScene startOnScene)
+                    foreach (var s in a.GetAllSystems)
                     {
-                        startOnScene.StartOnScene();
+                        if (s is IStartOnScene startOnScene)
+                        {
+                            startOnScene.StartOnScene();
+                        }
                     }
                 }
             }
