703990246b9a325a0effc1a7296f7a156f25abe6
Brightori <el.brightori@gmail.com>
2022-11-30 23:21:59 +0300

    upd: abilities

--- Abilities/AbilitiesHolderComponent.cs
+++ Abilities/AbilitiesHolderComponent.cs
@@ -1,10 +1,21 @@
-﻿using HECSFramework.Core;
+﻿using System.Collections;
+using System.Linq;
+using HECSFramework.Core;
 using HECSFramework.Unity;
+using HECSFramework.Unity.Helpers;
+using Sirenix.OdinInspector;
 
 namespace Components
 {
     public sealed partial class AbilitiesHolderComponent : BaseComponent
     {
+        [ValueDropdown("GetAbilities", IsUniqueList = true)]
         public EntityContainer[] AbilitiesContainers = new EntityContainer[0];
+
+
+        private IEnumerable GetAbilities()
+        {
+            return new SOProvider<EntityContainer>().GetCollection().Where(x => x.IsHaveComponent<AbilityTagComponent>());
+        }
     }
 }
--- Abilities/AbilitiesSystem.cs
+++ Abilities/AbilitiesSystem.cs
@@ -1,11 +1,20 @@
-﻿using Components;
+﻿using Commands;
+using Components;
 using HECSFramework.Core;
 using HECSFramework.Unity;
 
 namespace Systems
 {
-    public sealed partial class AbilitiesSystem : BaseSystem
+    public sealed partial class AbilitiesSystem : BaseSystem, IReactCommand<ViewReadyCommand>
     {
+        public void CommandReact(ViewReadyCommand command)
+        {
+            foreach (var a in abilitiesHolderComponent.Abilities)
+            {
+                a.Command(command);
+            }
+        }
+
         partial void ClientInit()
         {
             foreach (var abilityContainer in abilitiesHolderComponent.AbilitiesContainers)
