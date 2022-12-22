bdf057a166dd79b3c6e3ff44a10d160d8dbefa3a
Brightori <el.brightori@gmail.com>
2022-12-22 14:27:22 +0300

    upd: sound volume now global

--- Sound/SoundVolumeComponent.cs
+++ Sound/SoundVolumeComponent.cs
@@ -1,12 +1,11 @@
-using HECSFramework.Core;
-using HECSFramework.Unity;
 using System;
+using HECSFramework.Core;
 using UnityEngine;
 
 namespace Components
 {
     [Serializable][Documentation(Doc.Sound, "This holds volume settings for music and fx")]
-    public sealed class SoundVolumeComponent : BaseComponent
+    public sealed class SoundVolumeComponent : BaseComponent, IWorldSingleComponent
     {
         [Range(0, 1)] public float MusicVolume = 0.6f;
         [Range(0, 1)] public float SoundVolume = 1.0f;
