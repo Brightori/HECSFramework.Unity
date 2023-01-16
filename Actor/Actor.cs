8a8e5ef85a77679e1aa46e0f684cbd0c5f054504
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-01-16 19:25:24 +0300

    upd: actor

--- Actor/Actor.cs
+++ Actor/Actor.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using Components;
 using HECSFramework.Core;
 using Sirenix.OdinInspector;
 using Systems;
@@ -10,56 +8,31 @@ using UnityEngine;
 namespace HECSFramework.Unity
 {
     [DefaultExecutionOrder(-100)]
-    public partial class Actor : MonoBehaviour, IActor, IHECSDisable, IHECSEnable
+    public partial class Actor : MonoBehaviour, IActor
     {
         [SerializeField, BoxGroup("Init")] private ActorInitModule actorInitModule = new ActorInitModule();
         [SerializeField, BoxGroup("Init")] private ActorContainer actorContainer;
 
-        private Entity entity = new Entity();
+        private Entity entity;
         public GameObject GameObject => gameObject;
-       
+
         public string ContainerID => entity.ContainerID;
         public ActorContainer ActorContainer => actorContainer;
 
+        public IEntity Entity => entity;
 
-        private HECSMask actorContainerMask = HMasks.GetMask<ActorContainerID>();
-
-      
         public void Command<T>(T command) where T : struct, ICommand => entity.Command(command);
 
         private void Awake()
         {
-            actorInitModule.InitModule(this);
+            entity = new Entity(EntityManager.Worlds.Data[actorInitModule.WorldIndex], gameObject.name);
+            entity.SetGuid(actorInitModule.Guid);
 
             if (actorInitModule.InitActorMode == InitActorMode.InitOnStart)
             {
-                CreateEntity();
-
-                if (actorContainer != null && !IsInited)
-                    actorContainer.Init(this);
+                if (actorContainer != null && !entity.IsInited)
+                    actorContainer.Init(this.entity);
             }
-        }
-
-        private void CreateEntity()
-        {
-            if (entityCreated)
-                return;
-
-            entity.SetID(actorInitModule.ID);
-            entity.SetWorld(actorInitModule.WorldIndex);
-            entity.SetGuid(actorInitModule.Guid);
-            entityCreated = true;
-        }
-
-        private void CreateEntity(World world)
-        {
-            if (entityCreated)
-                return;
-
-            entity.SetID(actorInitModule.ID);
-            entity.SetWorld(world);
-            entity.SetGuid(actorInitModule.Guid);
-            entityCreated = true;
         }
 
         public void InitWithContainer()
@@ -68,57 +41,15 @@ namespace HECSFramework.Unity
             Init();
         }
 
+        public void Init(World world = null)
+        {
+            entity.Init(world);
+        }
+
         public void InitWithContainer(ActorContainer entityContainer)
         {
             actorContainer = entityContainer;
             InitWithContainer();
-        }
-
-        public void Init(bool needRegister = true)
-        {
-            if (entity == null)
-                CreateEntity();
-
-            if (World == null)
-                entity.SetWorld(0);
-
-            if (!IsInited && actorContainer != null && !entity.ContainsMask(ref actorContainerMask))
-                actorContainer.Init(this);
-
-            entity.InitComponentsAndSystems();
-
-            if (needRegister)
-                EntityManager.RegisterEntity(this, true);
-
-            GetOrAddComponent<TransformComponent>(this);
-            GetOrAddComponent<UnityTransformComponent>(this);
-
-            entity.AfterInit();
-
-            if (needRegister)
-            {
-                for (int i = 0; i < ComponentsMask.CurrentIndexes.Count; i++)
-                {
-                    var c = entity.GetAllComponents[ComponentsMask.CurrentIndexes[i]];
-
-                    if (c.IsRegistered)
-                        continue;
-
-                    World?.AddOrRemoveComponent(c, true);
-                    TypesMap.RegisterComponent(c.ComponentsMask.Index, c.Owner, true);
-                    c.SetIsRegistered();
-                }
-            }
-
-            entity.SetIsInited();
-        }
-
-        public void Init(int worldIndex, bool needRegister = true)
-        {
-            if (!entityCreated)
-                CreateEntity(EntityManager.Worlds.Data[worldIndex]);
-
-            Init(needRegister);
         }
 
         protected virtual void Start()
@@ -142,29 +73,18 @@ namespace HECSFramework.Unity
 
         public void HecsDestroy()
         {
-            World.GetSingleSystem<DestroyEntityWorldSystem>().ProcessDeathOfActor(this);
+            entity.World.GetSingleSystem<DestroyEntityWorldSystem>().ProcessDeathOfActor(this);
         }
 
         private void OnDestroy()
         {
-            if (IsAlive)
+            if (entity.IsAlive)
                 entity.HecsDestroy();
         }
 
-        public void EntityDestroy() => entity.HecsDestroy();
-
-
-
-        public void InjectEntity(IEntity entity, IEntity owner = null, bool additive = false) => this.entity.InjectEntity(entity, this, additive);
-
-        public void Pause() => entity.Pause();
-        public void RemoveHecsComponent(IComponent component) => entity.RemoveHecsComponent(component);
-        public void RemoveHecsComponent(HECSMask component) => entity.RemoveHecsComponent(component);
-        public void RemoveHecsSystem(ISystem system) => entity.RemoveHecsSystem(system);
-
         public bool TryGetComponent<T>(out T component, bool lookInChildsToo = false)
         {
-            if (!IsAlive)
+            if (!entity.IsAlive())
             {
                 component = default;
                 return false;
@@ -186,54 +106,8 @@ namespace HECSFramework.Unity
             return components != null && components.Length > 0;
         }
 
-        protected virtual void Reset()
-        {
-            actorInitModule.SetID(gameObject.name);
-            actorInitModule.SetGuid(Guid.NewGuid());
-        }
-
-        public bool TryGetHecsComponent<T>(HECSMask mask, out T component) where T : IComponent =>
-            entity.TryGetHecsComponent(mask, out component);
-
-        public bool TryGetSystem<T>(out T system) where T : ISystem => entity.TryGetSystem(out system);
-
-        public void UnPause()
-        {
-            entity.UnPause();
-        }
-
-        public T GetOrAddComponent<T>(IEntity owner = null) where T : class, IComponent => entity.GetOrAddComponent<T>(this);
-
-        public void SetGuid(Guid guid)
-        {
-            actorInitModule.SetGuid(guid);
-            entity?.SetGuid(guid);
-        }
-
-        public void AddOrReplaceComponent(IComponent component, IEntity owner = null, bool silently = false)
-        {
-            entity.AddOrReplaceComponent(component, this, silently);
-        }
-
+       
         public bool ContainsMask<T>() where T : IComponent => entity.ContainsMask<T>();
-        public void RemoveHecsComponent<T>() where T : IComponent => entity.RemoveHecsComponent<T>();
-
-        public bool TryGetHecsComponent<T>(out T component) where T : IComponent => entity.TryGetHecsComponent<T>(out component);
-
-        public IEnumerable<T> GetComponentsByType<T>() => entity.GetComponentsByType<T>();
-
-        public void SetWorldIndex(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool ContainsMask(HECSMultiMask mask) => entity.ContainsMask(mask);
-
-        public bool ContainsAnyFromMask(FilterMask mask) => entity.ContainsAnyFromMask(mask);
-
-        public bool ContainsAnyFromMask(HECSMultiMask mask) => entity.ContainsAnyFromMask(mask);
-
-        public bool ContainsMask(FilterMask mask) => entity.ContainsMask(mask);
 
         public override int GetHashCode()
         {
@@ -245,35 +119,12 @@ namespace HECSFramework.Unity
             return entity.Equals(other);
         }
 
-        public bool RemoveHecsSystem<T>() where T : ISystem => entity.RemoveHecsSystem<T>();
-
-
-        /// <summary>
-        /// if u need actor on different world, u should set world before init from container
-        /// </summary>
-        /// <param name="world"></param>
-        public void SetWorld(World world = null)
-        {
-            if (world == null)
-                world = EntityManager.Default;
-
-            if (entity == null)
-                CreateEntity(world);
-
-            entity.SetWorld(world);
-        }
-
-        public void MigrateEntityToWorld(World world, bool needInit = true)
-        {
-            entity.MigrateEntityToWorld(world, needInit);
-        }
-
         public void InjectContainer(EntityContainer container, bool isAdditive = false)
         {
             var components = container.GetComponentsInstances();
             var systems = container.GetSystemsInstances();
 
-            entity.Inject(components, systems, isAdditive, this);
+            entity.Inject(components, systems, isAdditive);
         }
 
         public void InjectContainer(EntityContainer container, bool isAdditive = false, params IComponent[] additionalComponents)
@@ -284,66 +135,13 @@ namespace HECSFramework.Unity
             foreach (var additional in additionalComponents)
                 components.Add(additional);
 
-            entity.Inject(components, systems, isAdditive, this);
+            entity.Inject(components, systems, isAdditive);
         }
 
-        public void Inject(List<IComponent> components, List<ISystem> systems, bool isAdditive = false, IEntity owner = null)
+        protected virtual void Reset()
         {
-            entity.Inject(components, systems, isAdditive, this);
-        }
-
-        public void HECSDisable()
-        {
-            foreach (var ci in entity.ComponentsMask.CurrentIndexes)
-            {
-                TypesMap.RegisterComponent(ci, entity, false);
-            }
-
-            foreach (var s in entity.GetAllSystems)
-            {
-                if (s is IHECSDisable disable)
-                {
-                    disable.HECSDisable();
-                }
-            }
-
-            entity.Pause();
-            entity.World.RegisterEntity(this, false);
-        }
-
-        public void HECSEnable()
-        {
-            entity.UnPause();
-
-            entity.World.RegisterEntity(this, true);
-
-            foreach (var ci in entity.ComponentsMask.CurrentIndexes)
-            {
-                TypesMap.RegisterComponent(ci, entity, true);
-            }
-
-            foreach (var s in entity.GetAllSystems)
-            {
-                if (s is IHECSEnable enable)
-                {
-                    enable.HECSEnable();
-                }
-            }
-        }
-
-        public Entity ReplaceEntity(Entity entity)
-        {
-            if (entity.IsInited)
-                throw new Exception("entity for replace should be not inited");
-
-            entity.SetWorld(this.entity.World);
-            entity.SetGuid(this.entity.GUID);
-
-            Dispose();
-            this.entity = entity;
-            entity.ProcessSystemsAndComponentsWithActor(this);
-            
-            return entity;
+            actorInitModule.SetID(gameObject.name);
+            actorInitModule.SetGuid(Guid.NewGuid());
         }
     }
 }
