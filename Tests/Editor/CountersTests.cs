fb2d9eabc989d4a9ea47b420b02fa123fb6bac71
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-02-10 15:34:07 +0300

    upd: tests

---
+++ Tests/Editor/CountersTests.cs
@@ -0,0 +1,24 @@
+﻿using Components;
+using HECSFramework.Core;
+using NUnit.Framework;
+using Systems;
+
+internal class CountersTests
+{
+    [Test]
+    public void TestAddCounter()
+    {
+        EntityManager.RecreateInstance();
+        var entity = new Entity();
+        entity.AddComponent(new CountersHolderComponent());
+        entity.AddHecsSystem(new CountersHolderSystem());
+        entity.Init();
+        entity.AddComponent(new TestComponent());
+
+        EntityManager.Default.GlobalUpdateSystem.Update();
+
+        var counter = entity.GetComponent<CountersHolderComponent>().GetCounter<ICounter<float>>(10);
+
+        Assert.IsTrue(counter != null && counter.Value == 11);
+    }
+}
--- Tests/TestComponent.cs
+++ Tests/TestComponent.cs
@@ -1,14 +1,15 @@
-using HECSFramework.Core;
-using HECSFramework.Unity;
-using Strategies;
 using System;
-using UnityEngine;
+using HECSFramework.Core;
+using Strategies;
 
 namespace Components
 {
-    [Serializable][Documentation(Doc.NONE, "")]
-    public sealed class TestComponent : BaseComponent
+    [Serializable][Documentation(Doc.Test, "we use this component on tests purpose")]
+    public sealed class TestComponent : ModifiableFloatCounterComponent
     {
         public Strategy Strategy;
+
+        public override int Id => 10;
+        public override float SetupValue => 11;
     }
 }
