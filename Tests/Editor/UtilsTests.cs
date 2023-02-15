7782336ce6fceaa412ce6ff889bbabb38259561e
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-02-15 16:45:33 +0300

    add: test cases for utils and collections

---
+++ Tests/Editor/UtilsTests.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using HECSFramework.Core;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class UtilsTests
+{
+    [Test]
+    public void HECSListSwapRemoveTest()
+    {
+        var list = new HECSList<IndexTest>(6);
+
+        for (int i = 0; i < 16; i++)
+        {
+            list.Add(new IndexTest { Index = i });
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            list.RemoveSwap(list.Data[8], out _);
+        }
+
+        bool badRow = false;
+
+        for (int i = 0; i < 12; i++)
+        {
+            if (list.Data[i] == null)
+            {
+                badRow = true;
+                break;
+            }
+        }
+
+        Assert.IsTrue(list.Count == 12 && !badRow);
+    }
+
+    public class IndexTest
+    {
+        public int Index = 0;
+    }
+}
