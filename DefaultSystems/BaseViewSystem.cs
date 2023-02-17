0bb44808aef5bfd062a96df43618027f920a745e
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-02-17 18:32:35 +0300

    add: baseview system

---
+++ DefaultSystems/BaseViewSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HECSFramework.Core;
+
+namespace Systems
+{
+    [Serializable]
+    [Documentation(Doc.Visual, Doc.HECS, "this system its base for all systems what depends from view (when we spawn view separatly from actor), this system check when view ready")]
+    public abstract class BaseViewSystem : BaseSystem, IInitAferView
+    {
+        protected bool isReady;
+
+        public void InitAferView()
+        {
+            isReady = true;
+        }
+    }
+}
