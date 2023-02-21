c230bd27b2ca8c42bbf74e4d080100d1a508ac1c
Brightori <el.brightori@gmail.com>
2023-02-22 01:48:51 +0300

    upd: tests

--- Tests/TestComponent.cs
+++ Tests/TestComponent.cs
@@ -5,7 +5,24 @@ using Strategies;
 namespace Components
 {
     [Serializable][Documentation(Doc.Test, "we use this component on tests purpose")]
-    public sealed class TestComponent : ModifiableFloatCounterComponent, IAfterEntityInit, IWorldSingleComponent
+    public sealed class TestComponent : ModifiableFloatCounterComponent, IAfterEntityInit
+    {
+        public Strategy Strategy;
+
+        public override int Id => 10;
+        public override float SetupValue => 11;
+
+        public int InitCount = 0;
+
+        public void AfterEntityInit()
+        {
+            InitCount++;
+        }
+    }
+
+    [Serializable]
+    [Documentation(Doc.Test, "we use this component on tests purpose")]
+    public sealed class TestWorldSingleComponent : ModifiableFloatCounterComponent, IAfterEntityInit, IWorldSingleComponent
     {
         public Strategy Strategy;
 
