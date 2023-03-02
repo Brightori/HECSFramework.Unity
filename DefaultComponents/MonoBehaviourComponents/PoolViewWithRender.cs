b2b74b8e2cf0bd89fdb2f4f5541fc9c6e43b7077
Brightori <el.brightori@gmail.com>
2023-03-02 07:52:34 +0300

    add: pooling with disable enable renderer functionality fix: actor

---
+++ DefaultComponents/MonoBehaviourComponents/PoolViewWithRender.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using HECSFramework.Core;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Components
+{
+    [Documentation(Doc.Visual, Doc.Poolable, "this component we use for pool gameobject with disable renderer feature")]
+    public class PoolViewWithRender : MonoBehaviour
+    {
+        [SerializeField] private AssetReference assetReference;
+        
+        public AssetReference AssetReference 
+        { 
+            get
+            {
+                if (assetReference == null || string.IsNullOrEmpty(assetReference.AssetGUID))
+                    throw new Exception("this go doesnt have asset reference " + gameObject.name);
+
+                return assetReference;
+            }
+        }
+
+        private Renderer[] renderers;
+
+        private void Awake()
+        {
+            renderers = GetComponentsInChildren<Renderer>();
+        }
+
+        public void EnableRenderers()
+        {
+            foreach (var r in renderers)
+            {
+                r.enabled = true;
+            }
+        }
+
+        public void DisableRenderers()
+        {
+            foreach (var r in renderers)
+            {
+                r.enabled = false;
+            }
+        }
+    }
+}
---
+++ DefaultSystems/PoolingSystemPoolViewPart.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Components;
+using HECSFramework.Core;
+using UnityEngine.AddressableAssets;
+
+namespace Systems
+{
+    public delegate void ViewWithRendererCallBack(PoolViewWithRender poolViewWithRender);
+
+    public partial class PoolingSystem : BaseSystem
+    {
+        private Dictionary<AssetReference, Stack<PoolViewWithRender>> poolViews = new Dictionary<AssetReference, Stack<PoolViewWithRender>>(8);
+
+        public void GetViewWithRenderer(AssetReference assetReference, ViewWithRendererCallBack viewCallBack)
+        {
+            if (!poolViews.ContainsKey(assetReference))
+            {
+                poolViews.Add(assetReference, new Stack<PoolViewWithRender>(16));
+            }
+
+            if (poolViews.TryGetValue(assetReference, out var pool))
+            {
+                if (pool.TryPop(out var result))
+                {
+                    viewCallBack?.Invoke(result);
+                    return;
+                }
+            }
+            else
+                poolViews.Add(assetReference, new Stack<PoolViewWithRender>(16));
+
+            GetView(assetReference, viewCallBack);
+        }
+
+        public void Release(PoolViewWithRender poolViewWithRender)
+        {
+            if (poolViews.TryGetValue(poolViewWithRender.AssetReference, out var pool))
+            {
+                pool.Push(poolViewWithRender);
+            }
+            else
+                poolViews.Add(poolViewWithRender.AssetReference, new Stack<PoolViewWithRender>(16));
+        }
+
+        private async void GetView(AssetReference assetReference, ViewWithRendererCallBack viewWithRendererCallBack)
+        {
+            var needed = await assetReference.InstantiateAsync().Task;
+            var pooledView = needed.GetComponent<PoolViewWithRender>();
+            viewWithRendererCallBack?.Invoke(pooledView);
+        }
+    }
+}
