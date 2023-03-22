34233ebcf149338866b68c8a461f8397eeb6305c
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-03-22 11:12:25 +0300

    upd: world helpers with loading scene and prefabs

--- Helpers/WorldHelpers.cs
+++ Helpers/WorldHelpers.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Components;
 using HECSFramework.Core;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -8,7 +9,7 @@ using UnityEngine.ResourceManagement.ResourceProviders;
 
 namespace HECSFramework.Unity
 {
-    [Documentation(Doc.HECS, "This helper load location to world")]
+    [Documentation(Doc.HECS, "This helper load location to World")]
     public static partial class WorldHelpers
     {
         public static async Task<SceneInstance> LoadLocationToWorld(this World world, AssetReference location)
@@ -17,12 +18,55 @@ namespace HECSFramework.Unity
             InitGameObjects(world, scene.Scene.GetRootGameObjects());
             return scene;
         }
+        public static async Task<GameObject> LoadPrefabToWorld(this World world, AssetReference location)
+        {
+            var obj = await location.InstantiateAsync().Task;
+            InitGameObject(world, obj);
+            return obj;
+        }
 
         public static async Task<SceneInstance> LoadLocationToWorld(this World world, string location)
         {
             var scene = await Addressables.LoadSceneAsync(location, UnityEngine.SceneManagement.LoadSceneMode.Additive).Task;
             InitGameObjects(world, scene.Scene.GetRootGameObjects());
             return scene;
+        }
+
+        public static async Task<GameObject> LoadPrefabToWorld(this World world, string location)
+        {
+            var obj = await Addressables.InstantiateAsync(location).Task;
+            InitGameObject(world, obj);
+            return obj;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void InitGameObject(World world, GameObject gameObject)
+        {
+            List<Actor> actorsList = new List<Actor>(16);
+            var actors = gameObject.GetComponentsInChildren<Actor>();
+            foreach (var a in actors)
+            {
+                if (a.IsAlive())
+                {
+                    HECSDebug.LogError(
+                        "Actors on scene when we load it to World, should not be inited, remove init on start from them");
+                    continue;
+                }
+
+                a.Init(world, true, true);
+                actorsList.Add(a);
+            }
+
+            foreach (var a in actorsList)
+            {
+                foreach (var s in a.Entity.Systems)
+                {
+                    if (s is IStartOnScene startOnScene)
+                    {
+                        startOnScene.StartOnScene();
+                    }
+                }
+            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -38,7 +82,7 @@ namespace HECSFramework.Unity
                 {
                     if (a.IsAlive())
                     {
-                        HECSDebug.LogError("Actors on scene when we load it to world, should not be inited, remove init on start from them");
+                        HECSDebug.LogError("Actors on scene when we load it to World, should not be inited, remove init on start from them");
                         continue;
                     }
 
