a7e385ffa032bbfe7bbb78b09843156d80f87872
Brightori <el.brightori@gmail.com>
2023-03-26 17:18:41 +0300

    add: has component standart predicate

---
+++ Predicates/HasComponentPredicate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using HECSFramework.Core;
+using HECSFramework.Unity;
+using Sirenix.OdinInspector;
+
+namespace Predicates
+{
+    public sealed partial class HasComponentPredicate : IPredicate
+    {
+        [ValueDropdown("SetIndexFromBP")]
+        [LabelText("Component")]
+        [ShowInInspector]
+        public int SetIndex { get => Index; set=> Index = value; }
+
+        private IEnumerable SetIndexFromBP()
+        {
+            var bpProvider = new BluePrintsProvider();
+            var list = new ValueDropdownList<int>();
+
+            foreach (var bp in bpProvider.Components)
+            {
+                list.Add(bp.Key.Name, IndexGenerator.GenerateIndex(bp.Key.Name));
+            }
+
+            return list;
+        }
+    }
+}
