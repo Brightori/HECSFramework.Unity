127a46509cb47bdc180f9796ef32b5343b287161
Brightori <el.brightori@gmail.com>
2023-04-13 01:18:14 +0300

    upd: baseview system now check we ready or not on after init entity

--- DefaultSystems/BaseViewSystem.cs
+++ DefaultSystems/BaseViewSystem.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using Components;
 using HECSFramework.Core;
 
 namespace Systems
 {
     [Serializable]
     [Documentation(Doc.Visual, Doc.HECS, "this system its base for all systems what depends from view (when we spawn view separatly from actor), this system check when view ready")]
-    public abstract class BaseViewSystem : BaseSystem, IInitAferView
+    public abstract class BaseViewSystem : BaseSystem, IInitAferView, IAfterEntityInit
     {
         protected bool isReady;
+
+        public virtual void AfterEntityInit()
+        {
+            if (Owner.ContainsMask<ViewReadyTagComponent>() || !Owner.ContainsMask<ViewReferenceGameObjectComponent>())
+                InitAferView();
+        }
 
         public void InitAferView()
         {
