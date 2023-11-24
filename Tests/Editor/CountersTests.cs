f2fb3a756fee080b9655175d3ca96442630e05c1
Brightori <el.brightori@gmail.com>
2023-11-24 11:42:06 +0300

    add: tests for modifiers

--- Tests/Editor/CountersTests.cs
+++ Tests/Editor/CountersTests.cs
@@ -1,7 +1,9 @@
-﻿using Components;
+﻿using System;
+using Components;
 using HECSFramework.Core;
 using NUnit.Framework;
 using Systems;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 internal class CountersTests
 {
@@ -35,5 +37,237 @@ internal class CountersTests
         entity.RemoveComponent<TestComponent>();
         var counter = entity.GetComponent<CountersHolderComponent>().GetCounter<ICounter<float>>(10);
         Assert.IsTrue(counter == null);
+    }
+
+    [Test]
+    public void TestAddModifier()
+    {
+        var check = new ModifiersFloatContainer();
+
+        check.SetBaseValue(2);
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Add,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Add,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        Assert.IsTrue(check.GetCalculatedValue() == 6);
+    }
+
+    [Test]
+    public void TestSubtractModifier()
+    {
+        var check = new ModifiersFloatContainer();
+
+        check.SetBaseValue(2);
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Subtract,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Subtract,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        Assert.IsTrue(check.GetCalculatedValue() == -2);
+    }
+
+    [Test]
+    public void TestMultiplyModifier()
+    {
+        var check = new ModifiersFloatContainer();
+
+        check.SetBaseValue(2);
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Multiply,
+            GetValue = 4,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Multiply,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        Assert.IsTrue(check.GetCalculatedValue() == 16);
+    }
+
+    [Test]
+    public void TestDivideModifier()
+    {
+        var check = new ModifiersFloatContainer();
+
+        check.SetBaseValue(16);
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Divide,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Divide,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        Assert.IsTrue(check.GetCalculatedValue() == 4);
+    }
+
+    [Test]
+    public void TestAllLevelsOfModifiers()
+    {
+        var check = new ModifiersFloatContainer();
+
+        check.SetBaseValue(3);
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Add,
+            GetValue = 3,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Add,
+            GetValue = 6,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Subtract,
+            GetValue = 3,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Subtract,
+            GetValue = 3,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Multiply,
+            GetValue = 3,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Multiply,
+            GetValue = 0.5f,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Divide,
+            GetValue = 0.5f,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Divide,
+            GetValue = 2f,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        Assert.IsTrue(check.GetCalculatedValue() == 9);
+    }
+
+
+    [Test]
+    public void TestAddSubtractModifier()
+    {
+        var check = new ModifiersFloatContainer();
+
+        check.SetBaseValue(2);
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Add,
+            GetValue = 5,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Subtract,
+            GetValue = 2,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        check.AddModifier(Guid.NewGuid(), new DefaultFloatModifier
+        {
+            GetCalculationType = ModifierCalculationType.Subtract,
+            GetValue = 3,
+            GetModifierType = ModifierValueType.Value,
+            ID = 1,
+            ModifierGuid = Guid.NewGuid(),
+        });
+
+        Assert.IsTrue(check.GetCalculatedValue() == 0);
     }
 }
