cc9050625308abaf8e9aefde97ace2d4dc7afb16
Brightori <el.brightori@gmail.com>
2024-02-13 03:36:38 +0300

    upd: over ui functionality

--- DefaultComponents/InputOverUIComponent.cs
+++ DefaultComponents/InputOverUIComponent.cs
@@ -1,13 +1,35 @@
 using System;
+using System.Collections.Generic;
 using HECSFramework.Core;
-using Systems;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Components
 {
-    [RequiredAtContainer(typeof(InputOverUISystem))]
-    [Serializable][Documentation(Doc.HECS, Doc.Input, "this component holds bool  - on this moment we over ui or not")]
-    public sealed partial class InputOverUIComponent : BaseComponent, IWorldSingleComponent
+    [Serializable]
+    [Documentation(Doc.HECS, Doc.Input, "this component holds bool  - on this moment we over ui or not")]
+    public sealed partial class InputOverUIComponent : BaseComponent, IWorldSingleComponent, IInitable
     {
-        public bool InputOverUI;
+        private bool isOverUI;
+        private int frameCount;
+        private List<RaycastResult> raycastResults = new List<RaycastResult>(32);
+        private PointerEventData pointerEventData;
+
+        public bool InputOverUI(Vector2 screenPos)
+        {
+            if (Time.frameCount == frameCount)
+                return isOverUI;
+
+            pointerEventData.position = screenPos;
+            EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+            isOverUI = raycastResults.Count > 0;
+            frameCount = Time.frameCount;
+            return isOverUI;
+        }
+
+        public void Init()
+        {
+            pointerEventData = new PointerEventData(EventSystem.current);
+        }
     }
 }
