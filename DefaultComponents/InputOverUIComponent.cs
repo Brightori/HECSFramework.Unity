91a8656fd36210e795b0719668d03300104d8928
Brightori <el.brightori@gmail.com>
2024-02-14 00:55:45 +0300

    add: second variant for raycasting (without vector2 with input identifier )

--- DefaultComponents/InputOverUIComponent.cs
+++ DefaultComponents/InputOverUIComponent.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using HECSFramework.Core;
+using HECSFramework.Unity;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace Components
 {
@@ -10,10 +12,17 @@ namespace Components
     [Documentation(Doc.HECS, Doc.Input, "this component holds bool  - on this moment we over ui or not")]
     public sealed partial class InputOverUIComponent : BaseComponent, IWorldSingleComponent, IInitable
     {
+        [SerializeField]
+        private InputIdentifier inputIdentifier;
+
+        [NonSerialized]
+        public List<RaycastResult> raycastResults = new List<RaycastResult>(32);
+
         private bool isOverUI;
         private int frameCount;
-        private List<RaycastResult> raycastResults = new List<RaycastResult>(32);
         private PointerEventData pointerEventData;
+
+        private InputAction inputAction;
 
         public bool InputOverUI(Vector2 screenPos)
         {
@@ -27,9 +36,26 @@ namespace Components
             return isOverUI;
         }
 
+        public bool InputOverUI()
+        {
+            if (Time.frameCount == frameCount)
+                return isOverUI;
+
+            pointerEventData.position = inputAction.ReadValue<Vector2>();
+            EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+            isOverUI = raycastResults.Count > 0;
+            frameCount = Time.frameCount;
+            return isOverUI;
+        }
+
         public void Init()
         {
             pointerEventData = new PointerEventData(EventSystem.current);
+
+            if (inputIdentifier != null) 
+            { 
+                Owner.GetComponent<Components.InputActionsComponent>().TryGetInputAction(inputIdentifier.name, out inputAction);
+            }
         }
     }
 }
