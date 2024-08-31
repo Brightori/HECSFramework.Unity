20f0db815c294e420164ca99341cb32c636e25f8
Brightori <el.brightori@gmail.com>
2024-08-31 03:46:55 +0300

    upd: helpers functionality

---
+++ Helpers/GameHelpers/DateTimeHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HECSFramework.Core;
+
+namespace Helpers
+{
+    [Documentation(Doc.Helpers, Doc.HECS, Doc.Time, "this class contains helpers around datetime and time")]
+    public static class DateTimeHelpers
+    {
+        public static float TimeLeft(DateTime dateTime, float intervalInSeconds)
+        {
+            return (float)(DateTime.UtcNow - dateTime).TotalSeconds - intervalInSeconds;
+        }
+    }
+
+    [Documentation(Doc.Helpers, Doc.Time, "we can have realisation for providing time from additional source")]
+    public interface ITimeProvider
+    {
+        DateTime GetTime();
+    }
+}
