302b8a51e62f27177b61af563f29fc61adc6fc4f
Brightori <el.brightori@gmail.com>
2024-09-21 03:06:54 +0300

    add: default actions for animations

---
+++ Animations/Actions/SetAnimationBoolean.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Components;
+using HECSFramework.Core;
+
+[Serializable]
+public class SetAnimationBoolean : IAction
+{
+    public AnimatorParameterIdentifier TriggerParameter;
+    public bool Value;
+
+    public void Action(Entity entity)
+    {
+        if (entity.ContainsMask<AbilityTagComponent>())
+        {
+            entity.GetComponent<AbilityOwnerComponent>().AbilityOwner.Command(new Commands.BoolAnimationCommand { Index = TriggerParameter.Id, Value = this.Value });
+        }
+        else
+        {
+            entity.Command(new Commands.BoolAnimationCommand { Index = TriggerParameter.Id, Value = this.Value });
+        }
+    }
+}
---
+++ Animations/Actions/SetAnimationTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Components;
+using HECSFramework.Core;
+
+[Serializable]
+public class SetAnimationTrigger : IAction
+{
+    public AnimatorParameterIdentifier TriggerParameter;
+
+    public void Action(Entity entity)
+    {
+        if (entity.ContainsMask<AbilityTagComponent>())
+        {
+            entity.GetComponent<AbilityOwnerComponent>().AbilityOwner.Command(new Commands.TriggerAnimationCommand { Index = TriggerParameter.Id });
+        }
+        else
+        {
+            entity.Command(new Commands.TriggerAnimationCommand { Index = TriggerParameter.Id });
+        }
+    }
+}
