13c010d9c9e0a801ec018c7189a3a696beaa244b
Brightori <el.brightori@gmail.com>
2024-10-24 02:39:05 +0300

    upd: hecs sync pool

--- Helpers/HecsSyncPool.cs
+++ Helpers/HecsSyncPool.cs
@@ -9,7 +9,7 @@ namespace Helpers
         private GameObject prfb;
         private Stack<T> pool = new Stack<T>(4);
         public HECSList<T> Items = new HECSList<T>();
-
+        private Queue<T> releaseQueue = new Queue<T>();
         public HECSSyncPool(GameObject prfb)
         {
             this.prfb = prfb;
@@ -60,6 +60,31 @@ namespace Helpers
             Items.RemoveSwap(value);
             value.gameObject.SetActive(false);
             pool.Push(value);
+        }
+
+
+        /// <summary>
+        /// u should use ProcessReleaseQueue after this method, this method just add items what we want to release to queue 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="unparent"></param>
+        public void ReleaseWithQueue(T value, bool unparent = false)
+        {
+            if (unparent)
+                value.transform.SetParent(null);
+            
+            value.gameObject.SetActive(false);
+
+            releaseQueue.Enqueue(value);
+        }
+
+        public void ProcessReleaseQueue()
+        {
+            while (releaseQueue.TryDequeue(out var value))
+            {
+                Items.RemoveSwap(value);
+                pool.Push(value);
+            }
         }
 
         public void ReleaseAll(bool unparent = false)
