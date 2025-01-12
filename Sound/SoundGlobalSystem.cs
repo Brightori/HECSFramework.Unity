3f6bb6f0fe582e6ad04b493dc5977454eb2c2241
Brightori <el.brightori@gmail.com>
2025-01-12 17:36:07 +0300

    fix: sound global system

--- Sound/SoundGlobalSystem.cs
+++ Sound/SoundGlobalSystem.cs
@@ -12,7 +12,7 @@ namespace Systems
 {
     [Serializable, BluePrint]
     [Documentation(Doc.Audio, Doc.Global, Doc.HECS, "Default sound solution for HECS, its all about 2d sound on this moment")]
-    public sealed partial class SoundGlobalSystem : BaseSystem, ISoundGlobalSystem, IHaveActor, IReactEntity,
+    public sealed partial class SoundGlobalSystem : BaseSystem, ISoundGlobalSystem, IHaveActor,
         IReactGlobalCommand<PlaySoundCommand>,
         IReactGlobalCommand<StopSoundCommand>,
         IReactGlobalCommand<UpdateSoundOptionsCommand>,
