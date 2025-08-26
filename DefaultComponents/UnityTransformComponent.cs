82a88e08022336367a0adeaf6b6b1da8eb606e73
Brightori <el.brightori@gmail.com>
2025-08-26 14:58:08 +0300

    add: ihave position to unity transform component

--- DefaultComponents/UnityTransformComponent.cs
+++ DefaultComponents/UnityTransformComponent.cs
@@ -7,10 +7,12 @@ namespace Components
 {
     [Serializable]
     [Documentation(Doc.HECS, Doc.Actor, "This component provide access to unity transform, if this entity not actor, this component remove self")]
-    public sealed class UnityTransformComponent : BaseComponent, IHaveActor, IDisposable
+    public sealed class UnityTransformComponent : BaseComponent, IHaveActor, IDisposable, IHavePosition
     {
         public Actor Actor { get; set; }
-        
+
+        public Vector3 GetPosition => Transform.position;
+
         [NonSerialized]
         public Transform Transform;
 
