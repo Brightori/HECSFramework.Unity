18a65f2b92f1ea688ffb827094569ecc5ee9aac5
Brightori <el.brightori@gmail.com>
2025-09-04 21:36:40 +0300

    upd: debug

--- Debugger/HECSDebugUnitySide.cs
+++ Debugger/HECSDebugUnitySide.cs
@@ -6,31 +6,61 @@ namespace HECSFramework.Unity
     {
         public void LogDebug(string info)
         {
-#if DEBUG_LOG
+#if DEBUG_LOG && DEBUG_FRAMECOUNT
             UnityEngine.Debug.Log($"[{Time.frameCount}] {info}");
+#elif DEBUG_LOG
+            UnityEngine.Debug.Log(info);
 #endif
         }
 
         public void Log(string info)
         {
+#if DEBUG_FRAMECOUNT
             UnityEngine.Debug.Log($"[{Time.frameCount}] {info}");
+#else
+            UnityEngine.Debug.Log(info);
+#endif
         }
 
         public void LogError(string info)
         {
+#if DEBUG_FRAMECOUNT
             UnityEngine.Debug.LogError($"[{Time.frameCount}] {info}");
+#else
+            UnityEngine.Debug.LogError(info);
+#endif
         }
 
         public void LogWarning(string info)
         {
-            UnityEngine.Debug.LogWarning($"[{Time.frameCount}] {info}");
+#if DEBUG_FRAMECOUNT
+            UnityEngine.LogError.LogWarning($"[{Time.frameCount}] {info}");
+#else
+            UnityEngine.Debug.LogWarning(info);
+#endif
+        }
+
+        public void LogDebug(string info, Object context)
+        {
+#if DEBUG_LOG && DEBUG_FRAMECOUNT
+            UnityEngine.Debug.Log($"[{Time.frameCount}] {info}", context);
+#elif DEBUG_LOG
+            UnityEngine.Debug.Log(info, context);
+#endif
         }
 
         public void LogDebug(string info, object context)
         {
-#if DEBUG_LOG
-            UnityEngine.Debug.Log($"[{Time.frameCount}] {info}");
+#if DEBUG_LOG && DEBUG_FRAMECOUNT
+            UnityEngine.Debug.Log($"[{Time.frameCount}] {info} {context.ToString()}");
+#elif DEBUG_LOG
+            UnityEngine.Debug.Log($"{info} {context.ToString()}");
 #endif
         }
     }
+}
+
+public partial interface IDebugDispatcher
+{
+    void LogDebug(string info, UnityEngine.Object context);
 }
---
+++ Debugger/NullDebug.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public partial class NullDebug : IDebugDispatcher
+{
+    public void LogDebug(string info, Object context)
+    {
+        Debug.Log(info, context);
+    }
+}
