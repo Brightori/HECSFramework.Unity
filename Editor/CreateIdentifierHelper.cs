9a23e3cdf4964e32c2063a1c7b932d0cd1c24ffd
Brightori <el.brightori@gmail.com>
2026-01-30 16:46:10 +0300

    add: identifier type to identifier helper

--- Editor/CreateIdentifierHelper.cs
+++ Editor/CreateIdentifierHelper.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Components;
+using HECSFramework.Core.Generator;
 using HECSFramework.Unity;
 using HECSFramework.Unity.Editor;
 using Sirenix.OdinInspector;
@@ -11,11 +13,14 @@ using UnityEngine;
 
 public class CreateIdentifierHelper : OdinEditorWindow
 {
+    [BoxGroup("Instance")]
     [ValueDropdown(nameof(GetAllowedTypes))]
     public Type Type;
 
+    [BoxGroup("Instance")]
     public string Name;
 
+    [BoxGroup("Instance")]
     [Button]
     public void CreateIdentifier()
     {
@@ -36,7 +41,59 @@ public class CreateIdentifierHelper : OdinEditorWindow
 
         AssetDatabase.CreateAsset(so, pathAssetDataBase);
     }
+    
+    [BoxGroup("Type")]
+    public string TypeName;
+    
+    [BoxGroup("Type")]
+    [Button("Create")]
+    public void CreateIdentifierType()
+    {
+        if (TypeName == null || string.IsNullOrEmpty(TypeName))
+            return;
 
+        var identifierName = TypeName + "Identifier";
+        var maps = string.Empty;
+
+        maps += new UsingSyntax("HECSFramework.Unity", 1).ToString();
+        maps += new UsingSyntax("UnityEngine", 1).ToString();
+        
+        maps += GetContainersBody(identifierName);
+
+        SaveToFile(maps,identifierName);
+    }
+    
+    private static string GetContainersBody(string idName)
+    {
+        var tree = new TreeSyntaxNode();
+
+        tree.Add(new SimpleSyntax($"[CreateAssetMenu(fileName = \"{idName}\", menuName = \"Identifiers/{idName}\")]" + CParse.Paragraph));
+        tree.Add(new SimpleSyntax($"public class {idName} : IdentifierContainer" + CParse.Paragraph));
+        tree.Add(new LeftScopeSyntax());
+        tree.Add(new RightScopeSyntax());
+
+        return tree.ToString();
+    }
+    
+    private static void SaveToFile(string data, string fileName)
+    {
+        var pathToDirectory = InstallHECS.ScriptPath + InstallHECS.BluePrints+ InstallHECS.Identifiers;
+        var path = pathToDirectory + $"{fileName}.cs";
+    
+        try
+        {
+            if (!Directory.Exists(pathToDirectory))
+                Directory.CreateDirectory(pathToDirectory);
+    
+            File.WriteAllText(path, data);
+            var sourceFile2 = path.Replace(Application.dataPath, "Assets");
+            AssetDatabase.ImportAsset(sourceFile2);
+        }
+        catch
+        {
+            Debug.LogError("не смогли ослить " + pathToDirectory);
+        }
+    }
 
     [MenuItem("HECS Options/Helpers/Create Identifier Helper")]
     public static void ShowWindow()
