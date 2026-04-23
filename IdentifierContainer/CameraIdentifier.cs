5b898aaba097be61bfd42cda7e9081b8b24e9a03
Brightori <el.brightori@gmail.com>
2026-04-23 15:33:39 +0300

    mov: camera identifier

---
+++ IdentifierContainer/CameraIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using HECSFramework.Unity;
+using UnityEngine;
+
+namespace BluePrints.Identifiers
+{
+
+    [CreateAssetMenu(menuName = "Identifiers/CameraIdentifier", fileName = "CameraIdentifier", order = 0)]
+    public class CameraIdentifier : IdentifierContainer
+    {
+        
+    }
+}
