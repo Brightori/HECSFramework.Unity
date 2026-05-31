9f5ef2876797a6441ea8c1fb2237c8235e5f6b6c
Brightori <el.brightori@gmail.com>
2026-05-31 17:20:16 +0300

    fix: serialization of sound component

--- Sound/SoundVolumeComponent.cs
+++ Sound/SoundVolumeComponent.cs
@@ -7,16 +7,16 @@ namespace Components
     [Serializable][Documentation(Doc.Sound, "This holds volume settings for music and fx")]
     public sealed partial class SoundVolumeComponent : BaseComponent, IWorldSingleComponent
     {
-        [Field(0)]
+        [Field(1)]
         [Range(0, 1)]
         public float MasterVolume = 1.0f;
 
 
-        [Field(1)]
+        [Field(2)]
         [Range(0, 1)]
         public float MusicVolume = 0.6f;
 
-        [Field(2)]
+        [Field(3)]
         [Range(0, 1)] 
         public float SoundVolume = 1.0f;
     }
